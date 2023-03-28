bd5b979524bf421eb41ef0d014adaf1cf5700cad
Tiago Santos <105326524+TiagoSantos123@users.noreply.github.com>
2023-03-28 22:15:12 +0100

    Update Form1.Designer.cs

--- Projeto1LTI/Form1.Designer.cs
+++ Projeto1LTI/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Projeto1LTI
+namespace Projeto1LTI
 {
     partial class Form1
     {
@@ -31,6 +31,13 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.listarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.interfacesWirelessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.interfacesBridgeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rotasEstáticasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.endereçosIpsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.servidoresDHCPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.regrasDeFirewallToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,14 +48,20 @@
             this.toolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(332, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(475, 28);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.listarToolStripMenuItem});
+            this.listarToolStripMenuItem,
+            this.interfacesWirelessToolStripMenuItem,
+            this.interfacesBridgeToolStripMenuItem,
+            this.rotasEstáticasToolStripMenuItem,
+            this.endereçosIpsToolStripMenuItem,
+            this.servidoresDHCPToolStripMenuItem,
+            this.regrasDeFirewallToolStripMenuItem});
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
             this.toolStripMenuItem1.Size = new System.Drawing.Size(58, 24);
             this.toolStripMenuItem1.Text = "Listar";
@@ -56,15 +69,67 @@
             // listarToolStripMenuItem
             // 
             this.listarToolStripMenuItem.Name = "listarToolStripMenuItem";
-            this.listarToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.listarToolStripMenuItem.Text = "Interfaces";
+            this.listarToolStripMenuItem.Size = new System.Drawing.Size(254, 26);
+            this.listarToolStripMenuItem.Text = "Interfaces do dispositivo";
             this.listarToolStripMenuItem.Click += new System.EventHandler(this.listarToolStripMenuItem_Click);
+            // 
+            // interfacesWirelessToolStripMenuItem
+            // 
+            this.interfacesWirelessToolStripMenuItem.Name = "interfacesWirelessToolStripMenuItem";
+            this.interfacesWirelessToolStripMenuItem.Size = new System.Drawing.Size(254, 26);
+            this.interfacesWirelessToolStripMenuItem.Text = "Interfaces Wireless";
+            this.interfacesWirelessToolStripMenuItem.Click += new System.EventHandler(this.interfacesWirelessToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(403, 308);
+            this.listBox1.TabIndex = 2;
+            // 
+            // interfacesBridgeToolStripMenuItem
+            // 
+            this.interfacesBridgeToolStripMenuItem.Name = "interfacesBridgeToolStripMenuItem";
+            this.interfacesBridgeToolStripMenuItem.Size = new System.Drawing.Size(254, 26);
+            this.interfacesBridgeToolStripMenuItem.Text = "Interfaces Bridge";
+            this.interfacesBridgeToolStripMenuItem.Click += new System.EventHandler(this.interfacesBridgeToolStripMenuItem_Click);
+            // 
+            // rotasEstáticasToolStripMenuItem
+            // 
+            this.rotasEstáticasToolStripMenuItem.Name = "rotasEstáticasToolStripMenuItem";
+            this.rotasEstáticasToolStripMenuItem.Size = new System.Drawing.Size(254, 26);
+            this.rotasEstáticasToolStripMenuItem.Text = "Rotas Estáticas";
+            this.rotasEstáticasToolStripMenuItem.Click += new System.EventHandler(this.rotasEstáticasToolStripMenuItem_Click);
+            // 
+            // endereçosIpsToolStripMenuItem
+            // 
+            this.endereçosIpsToolStripMenuItem.Name = "endereçosIpsToolStripMenuItem";
+            this.endereçosIpsToolStripMenuItem.Size = new System.Drawing.Size(254, 26);
+            this.endereçosIpsToolStripMenuItem.Text = "Endereços Ips";
+            this.endereçosIpsToolStripMenuItem.Click += new System.EventHandler(this.endereçosIpsToolStripMenuItem_Click);
+            // 
+            // servidoresDHCPToolStripMenuItem
+            // 
+            this.servidoresDHCPToolStripMenuItem.Name = "servidoresDHCPToolStripMenuItem";
+            this.servidoresDHCPToolStripMenuItem.Size = new System.Drawing.Size(254, 26);
+            this.servidoresDHCPToolStripMenuItem.Text = "Servidores DHCP";
+            this.servidoresDHCPToolStripMenuItem.Click += new System.EventHandler(this.servidoresDHCPToolStripMenuItem_Click);
+            // 
+            // regrasDeFirewallToolStripMenuItem
+            // 
+            this.regrasDeFirewallToolStripMenuItem.Name = "regrasDeFirewallToolStripMenuItem";
+            this.regrasDeFirewallToolStripMenuItem.Size = new System.Drawing.Size(254, 26);
+            this.regrasDeFirewallToolStripMenuItem.Text = "Regras de Firewall";
+            this.regrasDeFirewallToolStripMenuItem.Click += new System.EventHandler(this.regrasDeFirewallToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 322);
+            this.ClientSize = new System.Drawing.Size(475, 413);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -80,6 +145,13 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem listarToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem interfacesWirelessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem interfacesBridgeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rotasEstáticasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem endereçosIpsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem servidoresDHCPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem regrasDeFirewallToolStripMenuItem;
     }
 }
 
